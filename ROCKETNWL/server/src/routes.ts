3c27d2f42c9b173004d6e543b9fe9d0979a4f638
Wilton <wilton00770@gmail.com>
2020-08-05 19:02:32 -0400

    aula 2

--- ROCKETNWL/server/src/routes.ts
+++ ROCKETNWL/server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import ClassController from './controllers/ClassesController';
+import ConnectionsControllers from './controllers/ConnectionsControllers';
 
 const routes = express.Router();
+const classesControllers = new ClassController();
+const connectionsControllers = new ConnectionsControllers();
 
-routes.get('/', (request,response) => {
-  
-  return response.json({message : "Falaaa dev!"});
-});
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsControllers.index);
+routes.post('/connections', connectionsControllers.create);
 
 export default routes;
